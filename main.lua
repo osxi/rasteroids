03c17ee624db530d50a25133cc1fe8732f5cb1a8
osxi <osxi@users.noreply.github.com>
2014-09-05 00:25:39 -0500

    Added background image; minimal main and mostly-complete conf.lua.

---
+++ main.lua
@@ -0,0 +1,14 @@
+
+-- initialization stuff
+function love.load()
+   background = love.graphics.newImage("assets/background.png")
+end
+
+-- main
+function love.draw()
+    for i = 0, love.graphics.getWidth() / background:getWidth() do
+        for j = 0, love.graphics.getHeight() / background:getHeight() do
+            love.graphics.draw(background, i * background:getWidth(), j * background:getHeight())
+        end
+    end
+end
