e6c246258720f71ef8a332970cf8694745cafd95
osxi <osxi@users.noreply.github.com>
2014-09-09 19:05:25 -0500

    Added default font; General clean-up.

--- main.lua
+++ main.lua
@@ -1,13 +1,22 @@
 -- initialization stuff
 function love.load()
-   background = love.graphics.newImage("assets/background.png")
+  -- Specify background image
+  background = love.graphics.newImage("assets/background.png")
+
+  -- Set font
+  love.graphics.setNewFont('assets/Inconsolata for Powerline.otf', 28)
+
+  -- Initialize score
+  score = 0
 end
 
 -- main
 function love.draw()
-    for i = 0, love.graphics.getWidth() / background:getWidth() do
-        for j = 0, love.graphics.getHeight() / background:getHeight() do
-            love.graphics.draw(background, i * background:getWidth(), j * background:getHeight())
-        end
+  for i = 0, love.graphics.getWidth() / background:getWidth() do
+    for j = 0, love.graphics.getHeight() / background:getHeight() do
+      love.graphics.draw(background, i * background:getWidth(), j * background:getHeight())
     end
+  end
+
+  love.graphics.print('Score: ' .. score, 5, 0)
 end
