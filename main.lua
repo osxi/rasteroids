6f4ec70fef78f97f557e7d6674621ecb15e616d5
osxi <osxi@users.noreply.github.com>
2014-11-04 20:37:48 -0600

    Edge of screen wrapping; Add temporary strafing instead of 360 degree turning.

--- main.lua
+++ main.lua
@@ -20,12 +20,28 @@ end
 -- update
 function love.update(dt)
   -- User input
-    if love.keyboard.isDown('w') then
-      origin[2] = origin[2] - 1
-    end
-    if love.keyboard.isDown('s') then
-      origin[2] = origin[2] + 1
-    end
+  if love.keyboard.isDown('w') then     -- Forward
+    origin[2] = origin[2] - 5
+  elseif love.keyboard.isDown('s') then -- Backward
+    origin[2] = origin[2] + 5
+  end
+  if love.keyboard.isDown('a') then     -- Strafe left
+    origin[1] = origin[1] - 5
+  elseif love.keyboard.isDown('d') then -- Strafe right
+    origin[1] = origin[1] + 5
+  end
+
+  -- Edge of screen wrapping
+  if origin[2] < -25 then                              -- Top of window
+    origin[2] = love.graphics.getHeight()
+  elseif origin[2] > love.graphics.getHeight()+25 then -- Bottom of window
+    origin[2] = 0
+  end
+  if origin[1] < -25 then                              -- Left side of window
+    origin[1] = love.graphics.getWidth()
+  elseif origin[1] > love.graphics.getWidth()+25 then -- Right side of window
+    origin[1] = 0
+  end
 
   -- Update player character polygon
   pc_vertices = { origin[1] + 25, origin[2] + 25, origin[1], origin[2] - 25,
